bacfb6ed7199a5a22b0fe3fb0823f0162e19b0bb
MADEPADIO <madepadio99@gmail.com>
2024-01-11 02:00:05 +0100

    new commit.

---
+++ organisationalChart/Chart.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace organisationalChart
+{
+    internal class Chart
+    {
+        private List<Staff> staffList;
+
+        public Chart()
+        {
+            staffList = new List<Staff>();
+        }
+
+        public void AddStaff(Staff staff, Staff reportsTo)
+        {
+            staff.ReportsTo = reportsTo;
+            staffList.Add(staff);
+        }
+
+        public void DisplayChart()
+        {
+            foreach (var staff in staffList)
+            {
+                Console.WriteLine($"{staff} reports to {staff.ReportsTo?.Name ?? "Nobody"}");
+            }
+        }
+    }
+}
